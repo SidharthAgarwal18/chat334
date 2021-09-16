1b1d170cb1aae64690c8a4447554ca38075aad91
SidharthAgarwal18 <sidharthagarwal2001@gmail.com>
2021-09-16 12:26:56 +0530

    finished

--- client.py
+++ client.py
@@ -17,35 +17,47 @@ class Client:
         self.create_connection(host_name,port_num,username)
 
     def register(self,username,send_recv):
-    	socket_registered = False
+    	
+        if send_recv=="TOSEND ":
+            self.sokS.send(('REGISTER '+send_recv+username+'\n\n').encode()) 
+        else:
+            self.sokR.send(('REGISTER '+send_recv+username+'\n\n').encode())
 
-    	while not socket_registered:
-    		self.sok.send(('REGISTER '+send_recv+username+'\n \n').encode()) 
+        while (True):
 
-    		while True:
-    			ack = self.sok.recv(1024).decode()
-    			if ("ERROR 100" in ack):
-    				print('ERROR 100 Malformed username\n \n')
-    				sys.exit()
-    				break
-    			if ack == "REGISTERED "+send_recv+username+"\n \n":
-    				socket_registered = True
-    				break
+            if send_recv=="TOSEND ":
+                ack = self.sokS.recv(1024).decode()
+            else:
+                ack = self.sokR.recv(1024).decode()
+
+            if ("ERROR 100 " in ack):
+                print('ERROR 100 Malformed username\n\n')
+                sys.exit()
+
+            if ("ERROR 101 " in ack):
+                print("ERROR 101 No user registered\n\n")
+                break
+
+            if ack == "REGISTERED "+send_recv+username+"\n\n":
+                break
 
     def create_connection(self,host,port,username):
-        self.sok = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        
+        self.sokR = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.sokS = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+                
         while (True):
             try:
-                self.sok.connect((host,int(port)))                
+                self.sokR.connect((host,int(port)))
+                self.sokS.connect((host,int(port)))              
                 break
             except:
                 print("Couldn't connect to given host number and port number")  
                 sys.exit()     
 
         self.username = username
-        self.register(username,"TOSEND ")
-        #self.register(username,"TORECV ")
+
+        self.register(username,"TORECV ")
+        self.register(username,"TOSEND ")        
 
         message_handler = threading.Thread(target=self.handle_messages,args=())
         message_handler.start()
@@ -55,23 +67,14 @@ class Client:
 
     def handle_messages(self):
         while True:
-            rec_message = self.sok.recv(1204).decode()
+            rec_message = self.sokR.recv(1204).decode()
 
             if rec_message=="":
-                continue
-            
-            if "ERROR 102" in rec_message:
-                print("ERROR 102 Unable to send\n\n")
-                continue
+                continue   
 
-            if "ERROR 103" in rec_message:
-                print("ERROR 103 Header Incomplete\n\n")
-                self.sok.close()
+            if rec_message[0:8] != "FORWARD ":
+                self.sokR.send(("ERROR 103 Header Incomplete\n\n").encode())
                 continue
-
-            if "SEND" in rec_message:
-                print("Message to "+ rec_message[5:(len(rec_message)-2)]+" successfully delivered")  
-                continue    
 
             rec_message = rec_message[8:]
             sender = ""
@@ -81,27 +84,41 @@ class Client:
                     break
                 sender = sender + char
 
+            if rec_message[len(sender):(len(sender)+16)]!= "\nContent-length:":
+                self.sokR.send(("ERROR 103 Header Incomplete\n\n").encode())
+                continue
+
             rec_message = rec_message[(len(sender)+16):]
             len_message = ""
+
+            validity = True
 
             for char in rec_message:
                 if char=='\n':
                     break
+                validity = ((ord(char) - 48 >=0) and (ord(char)-48<10))
+                if not validity:
+                    break
                 len_message = len_message + char
 
-            if sender=="" or len_message=="":
-                self.sok.send(("ERROR 103 Header Incomplete\n\n").encode())
+            if sender=="" or len_message=="" or (not validity):
+                self.sokR.send(("ERROR 103 Header Incomplete\n\n").encode())
                 continue
 
             len_int = int(len_message,10)
+
+            if rec_message[len(len_message):(len(len_message)+2)] != '\n\n':
+                self.sokR.send(("ERROR 103 Header Incomplete\n\n").encode())
+                continue
+
             rec_message = rec_message[(len(len_message)+2):]
 
             if len(rec_message)!= len_int:
-                self.sok.send(("ERROR 103 Header Incomplete\n\n").encode())
+                self.sokR.send(("ERROR 103 Header Incomplete\n\n").encode())
                 continue
 
             ack_from_reciever = "RECEIVED "+sender+"\n\n"
-            self.sok.send(ack_from_reciever.encode())
+            self.sokR.send(ack_from_reciever.encode())
             print(sender+'-> '+rec_message)
 
 
@@ -114,13 +131,34 @@ class Client:
             if(message[0]!="@" or text==""):
                 print('<Kindly please enter the message in the format @reciptent text, where text is non empty>\n')
                 continue
-            
-            #if(reciptent==self.username):
-            #    print('You cannot send a message to yourself')
-            #    continue
 
             encoded_message = "SEND "+reciptent+"\nContent-length:"+str(len(text))+"\n\n"+text
-            self.sok.send(encoded_message.encode())
+            self.sokS.send(encoded_message.encode())
+
+            while True:
+                rec_message = self.sokS.recv(1204).decode()
+
+                if rec_message=="":
+                    continue
+
+                if "ERROR 101 " in rec_message:
+                    print("ERROR 101 No user registered\n\n")
+                    break
+
+                if "ERROR 102 " in rec_message:
+                    print("ERROR 102 Unable to send\n\n")
+                    break
+
+                if "ERROR 103 " in rec_message:
+                    print("ERROR 103 Header Incomplete\n\n")
+                    print('Closing sockets..')
+                    self.sokR.close()
+                    self.sokS.close()
+                    break
+
+                if "SEND " in rec_message:
+                    print("Message to "+ rec_message[5:(len(rec_message)-2)]+" successfully delivered")  
+                    break 
 
 
 if(len(sys.argv)!=4):
--- server.py
+++ server.py
@@ -21,68 +21,80 @@ class Server:
         host = socket.gethostbyname(socket.gethostname())
         port = port_num
 
-        self.clients = []
+        self.clientsS = []
+        self.clientsR = []
 
         self.sok.bind((host,port))
-        self.sok.listen(100)                        #Ask about this...
+        self.sok.listen(100)                        #Total clients possible are 100
     
         print('Running server on host: '+str(host))
         print('Running server on port: '+str(port))
 
-        self.username_lookup = {}
-        self.socket_lookup = {}
+        self.username_lookupS = {}
+        self.socket_lookupS = {}
+
+        self.username_lookupR = {}
+        self.socket_lookupR = {}
 
         while True:
             c, addr = self.sok.accept()
 
             username = c.recv(1024).decode()
 
-            if("REGISTER TOSEND" in username):
-                username = username[16:(len(username)-3)]
+            if(("REGISTER TOSEND " in username) and ('\n\n' in username) and (username[16:(len(username)-2)] in self.socket_lookupR.keys())):
+                last_two = username[(len(username)-2):len(username)]
+                username = username[16:(len(username)-2)]
+
+                if(valid_username(username) and last_two=="\n\n"):
+                    print('New sender connection. Username: '+str(username))
+
+                    self.username_lookupS[c] = username
+                    self.socket_lookupS[username] = c
+
+                    self.clientsS.append(c)
+                    c.send(("REGISTERED TOSEND "+username+"\n\n").encode())
+
+                    threading.Thread(target=self.handle_client,args=(c,addr,)).start()
+                else:
+                    error_message = 'ERROR 100 Malformed username\n\n'
+                    c.send(error_message.encode())
                 
-            elif("REGISTER TORECV" in username):
-                print('Design decision of making only one socket....')
-                continue
+            elif("REGISTER TORECV " in username and '\n\n' in username):
+                last_two = username[(len(username)-2):len(username)]
+                username = username[16:(len(username)-2)]
+
+                if(valid_username(username)  and last_two=="\n\n"):
+                    print('New reciever connection. Username: '+str(username))
+
+                    self.username_lookupR[c] = username
+                    self.socket_lookupR[username] = c
+
+                    self.clientsR.append(c)
+                    c.send(("REGISTERED TORECV "+username+"\n\n").encode())
+
+                else:
+                    error_message = 'ERROR 100 Malformed username\n\n'
+                    c.send(error_message.encode())
+
             else:
-                error_message = "ERROR 101 No user registered\n \n"
+                print('Illegal registration request...')
+                error_message = "ERROR 101 No user registered\n\n"
                 c.send((error_message).encode())
-
-            if(valid_username(username)):
-                print('New connection. Username: '+str(username))
-
-                self.username_lookup[c] = username
-                self.socket_lookup[username] = c
-
-                self.clients.append(c)
-                c.send(("REGISTERED TOSEND "+username+"\n \n").encode())
-
-                threading.Thread(target=self.handle_client,args=(c,addr,)).start()
-            else:
-                error_message = 'ERROR 100 Malformed username\n \n'
-                c.send(error_message.encode())
-
-
-    def broadcast(self,msg):
-        for connection in self.clients:
-            connection.send(msg.encode())
 
     def handle_client(self,c,addr):
         while True:
             try:
                 msg = c.recv(1024)
             except:
-                c.shutdown(socket.SHUT_RDWR)
-                self.clients.remove(c)                
-                print(str(self.username_lookup[c])+' left the room.')
+                c.shutdown(socket.SHUT_RDWR)          
+                print(str(self.username_lookupS[c])+' left the room.')
                 break
 
             if msg.decode() != '':
                 message = msg.decode()
 
-                if "SEND" not in message:
-                    sender_username = message[9:(len(message)-2)]
-                    ack_message = "SEND "+ self.username_lookup[c]+"\n\n"
-                    self.socket_lookup[sender_username].send(ack_message.encode())
+                if message[0:5] != "SEND ":
+                    c.send(("ERROR 103 Header Incomplete\n\n").encode())
                     continue
 
                 message = message[5:]
@@ -93,22 +105,50 @@ class Server:
                         break
                     rec_username = rec_username + char
 
-                if (rec_username not in (self.socket_lookup.keys()) and rec_username!="all"):
+                if (rec_username not in (self.socket_lookupS.keys()) and rec_username!="all"):
                     c.send(("ERROR 102 Unable to send\n\n").encode())
                     continue
 
-                message = "FORWARD "+ self.username_lookup[c] +message[len(rec_username):]
+                message = "FORWARD "+ self.username_lookupS[c] +message[len(rec_username):]
 
                 if rec_username!="all":
-                    self.socket_lookup[rec_username].send(message.encode())
+                    self.socket_lookupR[rec_username].send(message.encode())
+
+                    while True:
+                        ack_message = (self.socket_lookupR[rec_username].recv(1024)).decode()
+
+                        if ack_message!="":
+
+                            if "ERROR " in ack_message:
+                                c.send(ack_message.encode())
+                                break
+                            else:
+                                ack_message = "SEND "+ rec_username+"\n\n"
+                                c.send(ack_message.encode())
+                                break
                 else:
-                    for username in self.socket_lookup.keys():
-                        if username!=self.username_lookup[c]:
-                            self.socket_lookup[username].send(message.encode())
+                    broadcast_failed = False
 
+                    for allusername in self.socket_lookupS.keys():
+                        if allusername!=self.username_lookupS[c]:
 
+                            self.socket_lookupR[allusername].send(message.encode())
 
+                            while True:
+                                ack_message = (self.socket_lookupR[allusername].recv(1024)).decode()
 
+                                if ack_message!="":
+
+                                    if "ERROR " in ack_message:
+                                        broadcast_failed = True
+                                    
+                                    break
+                    
+                    if broadcast_failed:
+                        ack_message = "ERROR 103 Header Incomplete\n\n"
+                    else:
+                        ack_message = "SEND all\n\n"
+                    c.send(ack_message.encode())
 
 if (len(sys.argv)!=2):
     print('Kindly please correctly enter the port number')
